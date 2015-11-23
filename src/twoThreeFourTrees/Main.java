625ae0c209555ef0620bc9becd4db73b990b4652
Mickaël Bernardini <contact@mickael-bernardini.fr>
2015-11-23 14:38:33 +0100

    End console cosmetic, maybe a gui ? (we'll see)

--- src/twoThreeFourTrees/Main.java
+++ src/twoThreeFourTrees/Main.java
@@ -13,22 +13,23 @@ public class Main {
 		TwoThreeFourTrees tree = new TwoThreeFourTrees();
 		for (int i = 0; i < n; i++) {
 			Integer number = rand.nextInt(n);
-			NodeSearch search = tree.search(number);
-			if (search.getNode() != null) {
+			if (tree.search(number).getNode() != null) {
 				tree.remove(number);
-				System.out.println("The number " + number + " is in the tree, we REMOVE it");
+				System.out.println("[REMOVE] The number " + number + " is in the tree");
 			} else {
 				tree.add(number);
-				System.out.println("The number  " + number + " is not in the tree, we ADD it");
+				System.out.println("[ADD] The number " + number + " is not in the tree");
 			}
 			tree.display();
-			System.out.println("--------------- NEXT ---------------");
+			if (i < n - 1)
+				System.out.println("--------------- NEXT ---------------");
 		}
 	}
-	
+
 	private static void test() {
 		TwoThreeFourTrees twoThreeFourTrees = new TwoThreeFourTrees();
-		//Integer[] array = {60, 40, 20, 10, 27, 30, 33, 36, 61, 41, 21, 11, 28, 31, 34, 37};
+		// Integer[] array = {60, 40, 20, 10, 27, 30, 33, 36, 61, 41, 21, 11,
+		// 28, 31, 34, 37};
 		twoThreeFourTrees.add(12);
 		twoThreeFourTrees.remove(12);
 		twoThreeFourTrees.add(32);
